d8e8ea8ede61d558fa321732f06b4ec4c20a0b62
Nonnyss <nonthapan.wong@gmail.com>
2024-03-26 16:45:30 +0700

    Card

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { IBM_Plex_Sans_Thai } from "next/font/google";
 import { getServerSession } from "next-auth";
 import Nav from "@/components/Nav";
 import NextAuthProvider from "@/providers/NextAuthProvider";
+import { ThemeProvider } from "@material-tailwind/react";
 const ibm = IBM_Plex_Sans_Thai({
   subsets: ["latin", "thai"],
   weight: ["100", "200", "300", "400", "500", "600", "700"],
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -17,19 +17,17 @@ export default async function Nav() {
         <Link href="/">Home</Link>
         <Link href="/booking">Camp</Link>
       </div>
-      <div className="flex gap-16">
-        <div>
+      <div className="flex gap-10">
+        <div className="flex items-center">
           <Link href="/booking">Booking</Link>
         </div>
-        
-          
+
         <Link
           href={session ? "/api/auth/signout" : "/api/auth/signin"}
           className="align-middle select-none font-poppin font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
         >
           {session ? "Sign Out" : "Sign-In"}
         </Link>
-        
       </div>
     </div>
   );
