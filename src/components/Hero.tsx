35a6d02a20a5ab97f330da8589ec73cb5d7abd97
Thepbordin <thepbordinjaiinsom@gmail.com>
2024-03-27 02:57:51 +0700

    fix hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,10 @@ import Select from "./booking/Select";
 export default function Hero() {
   return (
     <>
-      <div className="grid grid-cols-2 min-w-[100vh] min-h-[100vh]">
+      <div
+        className="flex flex-row w-screen h-screen justify-center lg:justify-start items-center p-0 lg:p-20 overflow-hidden"
+        style={{ backgroundImage: `url("/img/bg.png")` }}
+      >
         <Image
           alt="bg"
           src="/img/bg.png"
@@ -13,21 +16,18 @@ export default function Hero() {
           height={1080}
           className="absolute"
         />
-        <div className="relative bgcolor flex flex-col items-end top-56">
-          <div className="space-y-5">
-            <div className="font-poppin font-bold text-6xl flex ">
-              <div className="bg-gradient-to-r from-[#0B60B0] to-[#40A2D8] bg-clip-text text-transparent p-3">
+        <div className="relative bgcolor flex flex-col  ">
+          <div className="">
+            <div className="font-poppin font-bold text-6xl  ">
+              <div className="bg-gradient-to-r from-[#0B60B0] to-[#40A2D8] bg-clip-text text-transparent mb-3">
                 CAMPGROUNG{" "}
               </div>
-              <div className="p-3">Booking </div>
+              <div className="mb-3">Booking </div>
             </div>
-            <div className="font-poppin font-semibold text-2xl flex gap-5 ml-14 ">
+            <div className="font-poppin font-semibold text-2xl flex gap-5 ">
               <FaCampground className="" />
-              <div>Find Your Perfect Campsite Today!</div>
+              <div className="mb-3">Find Your Perfect Campsite Today!</div>
             </div>
-          </div>
-          <div className="mt-8 flex justify-center w-full">
-            {/* <Select></Select> */}
             <button className="bg-[#1066B3] hover:bg-[#3899D3] text-white font-bold py-2 px-4 rounded-full">
               จองเลย
             </button>
