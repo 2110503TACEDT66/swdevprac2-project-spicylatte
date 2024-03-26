65c44e69297aed9c77755fb1ecc163e5b668c729
--__-- <Kaitosoloplay1@gmail.com>
2024-03-26 18:39:09 +0700

    campcard

---
+++ src/app/(campinfo)/camplocation/page.tsx
@@ -0,0 +1,13 @@
+import Cardpanel from "@/components/Card/cardpanel";
+
+
+export default function hospital() {
+    return(
+        <main>
+          <div className="mt-40">
+            <Cardpanel></Cardpanel>
+          </div>
+           
+        </main>
+    )
+}
